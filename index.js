ff064db8c3765ae9ba33bc9688f8eaf333a4369d
masudrana-1 <masudrana58855@gmail.com>
2022-11-25 15:45:01 +0600

    set server and add mongodb

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+// const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+// mongodb 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.cjxkvs1.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Arrow Computer server is running');
+})
+
+app.listen(port, () => {
+    console.log(`Arrow computer server is running on port: ${port}`);
+})
